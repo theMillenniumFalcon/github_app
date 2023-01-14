bb1914f6e998ae915a71510a2cacc46012fa4069
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-01-14 15:31:30 +0530

    added svg components

---
+++ src/components/svg/delete-svg.tsx
@@ -0,0 +1,31 @@
+import React, { memo } from 'react'
+import Svg, { G, Rect } from 'react-native-svg'
+import { useTheme } from '../../hooks/useTheme'
+import { ISVGProps } from './props'
+
+export const DeleteSVG: React.FC<ISVGProps> = memo(
+    ({ color, width, height }) => {
+        const { colors } = useTheme()
+
+        return (
+            <Svg viewBox="0 0 12.728 12.728" width={width} height={height ?? width}>
+                <G transform="translate(6.364 -4.95) rotate(45)">
+                    <Rect
+                        width="2"
+                        height="16"
+                        rx="1"
+                        transform="translate(7)"
+                        fill={color ?? colors.primary}
+                    />
+                    <Rect
+                        width="2"
+                        height="16"
+                        rx="1"
+                        transform="translate(16 7) rotate(90)"
+                        fill={color ?? colors.primary}
+                    />
+                </G>
+            </Svg>
+        )
+    }
+)
---
+++ src/components/svg/props.ts
@@ -0,0 +1,5 @@
+export interface ISVGProps {
+    color?: string
+    width?: string
+    height?: string
+}
